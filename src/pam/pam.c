dd111af59974eae76d8401e69d869b95e273406e
Iwan Timmer <irtimmer@gmail.com>
2016-05-02 16:49:39 +0200

    Add missing pam_sm_acct_mgmt

--- src/pam/pam.c
+++ src/pam/pam.c
@@ -70,6 +70,10 @@ static int get_count(const char* user, int amount) {
   return count;
 }
 
+PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+  return PAM_SUCCESS;
+}
+
 PAM_EXTERN int pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char **argv) {
   return PAM_SUCCESS;
 }
